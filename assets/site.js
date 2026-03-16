cd10f9e14c1c03fc2dddc51d06604a8e88981ed1
carpetcleaningcompany007-maker <carpetcleaningcompany007@gmail.com>
2026-03-16 01:32:32 +0000

    Delete assets directory

--- assets/site.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById('year')?.append(new Date().getFullYear());
-const io=new IntersectionObserver((entries)=>{
-  entries.forEach(entry=>{ if(entry.isIntersecting) entry.target.classList.add('show'); });
-},{threshold:.12});
-document.querySelectorAll('.reveal').forEach(el=>io.observe(el));
